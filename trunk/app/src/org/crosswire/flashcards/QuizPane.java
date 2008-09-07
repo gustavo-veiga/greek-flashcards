a316ea5edd5cdc81c81897701e7d9c1ac1cbf9ba
nobody <nobody@8f656a66-91e1-0310-b0ab-f5e3b1b95cab>
2008-09-07 10:48:35 +0000

    Added some changes to make dynamic building of packages easier

--- trunk/app/src/org/crosswire/flashcards/QuizPane.java
+++ trunk/app/src/org/crosswire/flashcards/QuizPane.java
@@ -174,7 +174,7 @@ public class QuizPane
 
      //Component initialization
      private void jbInit() {
-          choicesPanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 10));
+          choicesPanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 10, 10));
           startLessonButton.setText("Start");
           startLessonButton.addActionListener(new QuizPane_startLessonButton_actionAdapter(this));
 
